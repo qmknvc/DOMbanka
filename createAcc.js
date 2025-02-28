b17c9bddb97862a8f870076ea3b1e8ce973beaa7
Emir Mehinbasic <emir96meh@gmail.com>
2025-02-28 18:09:46 +0100

    Kreirao formu za kriranje acc, i dodo joj funkcionalnost i validaciju

---
+++ createAcc.js
@@ -0,0 +1,68 @@
+const form = document.getElementById("form");
+const password = document.getElementById("password");
+const newPassword = document.getElementById("new-password");
+const emailInput = document.querySelector("input[type='email']");
+const phoneInput = document.querySelector("input[type='tel']");
+const accountTypes = document.querySelectorAll("input[name='type-acc']");
+const submitButton = document.querySelector("button[type='submit']");
+const userDataDiv = document.getElementById("userData");
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault(); // Sprječava osvježavanje stranice
+
+  // Prikupljanje podataka
+  const name =document.querySelector("input[placeholder='Insert Name']").value.trim() ||"";
+  const surname =document.querySelector("input[placeholder='Insert Surname']").value.trim() || "";
+  const email = emailInput.value.trim() || "";
+  const phone = phoneInput.value.trim() || "";
+  const pass = password.value.trim() || "";
+  const confirmPass = newPassword.value.trim() || "";
+  let selectedAccountType = "";
+
+  accountTypes.forEach((radio) => {
+    if (radio.checked) {
+      selectedAccountType = radio.value; // Koristi value umjesto textContent
+    }
+  });
+
+  // Validacija
+  if (!name ||!surname ||!email ||!phone ||!pass ||!confirmPass ||!selectedAccountType) {
+    alert("Sva polja moraju biti popunjena!");
+    return;
+  }
+
+  if (!validateEmail(email)) {
+    alert("Unesite validnu e-mail adresu!");
+    return;
+  }
+
+  if (!validatePhone(phone)) {
+    alert("Unesite validan broj telefona! (Primjer: +38761123456)");
+    return;
+  }
+
+  if (pass.length < 8) {
+    alert("Lozinka mora imati najmanje 8 karaktera!");
+    return;
+  }
+
+  if (pass !== confirmPass) {
+    alert("Lozinke se ne podudaraju!");
+    return;
+  }
+
+  // Kreiranje objekta koji cuva informacije o useru 
+  const userData = {
+    name,
+    surname,
+    email,
+    phone,
+    accountType: selectedAccountType,
+  };
+
+  localStorage.setItem("userData", JSON.stringify(userData));
+
+  alert("Nalog uspješno kreiran!");
+  form.reset();
+});
+
