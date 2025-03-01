3b0dcd53dfa871ec6ae63cf5ed6edc59740c8191
qmknvc <alem.mknvc@gmail.com>
2025-03-01 23:19:47 +0100

    Dodan placeholder html za front page I ugradjen localstorage za accounts

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-/*Login dio - Alem*/ // ovaj dio koda radi samo ako je taj hmtl ocitan
-if (window.location.pathname == "/loginA.html"){
-const emailLogin    = document.querySelector("#emailLogin")
-const passLogin     = document.querySelector("#passLogin")
-const logInButton   = document.querySelector("#logInButton")
-const loginForm     = document.querySelector("#loginForm")
-
-logInButton.addEventListener("click", () => {
-    if(emailLogin.value != testChecking){
-        alert("Invalid email")
-        loginForm.reset()
-        return
-    }
-    if(passLogin.value != testChecking){
-        alert("Invalid password")
-        loginForm.reset()
-        return
-    }
-    
-})
-}
-/* Login dio zavrsen - Alem*/
-/* Create acc dio : */
+/* Create acc dio : - Alem*/
 if (window.location.pathname == "/createA.html"){ // ovaj dio koda radi samo ako je taj hmtl ocitan
 const emailCreate       = document.querySelector("#emailCreate")
 const passCreate        = document.querySelector("#passCreate")
@@ -34,11 +12,38 @@ function user(email, password){
     this.password = password
 }
 
-createAccButton.addEventListener("click", () => {
+createAccButton.addEventListener("click", (event) => {
+    event.preventDefault()
     const newUser = new user(emailCreate.value, passCreate.value)
     createdAccounts.push(newUser)
-    console.log(createdAccounts)
-    console.log(createdAccounts[0])
+
+    const registeredUsers = JSON.stringify(createdAccounts)
+    localStorage.setItem("users", registeredUsers)
 })
 }
 /* Create acc dio zavrsen*/
+/*Login dio - Alem*/
+if (window.location.pathname == "/loginA.html"){
+const emailLogin    = document.querySelector("#emailLogin")
+const passLogin     = document.querySelector("#passLogin")
+const logInButton   = document.querySelector("#logInButton")
+const loginForm     = document.querySelector("#loginForm")
+
+logInButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    if(emailLogin.value != 'test'){
+        alert("Invalid email")
+        emailLogin.value = ''
+        return
+    }
+    else if(passLogin.value != 'test'){
+        alert("Invalid password")
+        passLogin.value = ''
+        return
+    }
+    //window.location.href = "frontPage.html";
+    const newRegisteredUser = JSON.parse(localStorage.getItem("users"))
+    console.log(newRegisteredUser[0])
+})
+}
+/* Login dio zavrsen - Alem*/
